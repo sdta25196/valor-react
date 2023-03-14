170f1ad7de31c0ec8ec2e23fa10d781317b016c3
sdta25196 <451376882@qq.com>
2023-03-14 22:03:09 +0800

    feat: 初步完成useState

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -2,9 +2,9 @@ import { Dispatcher, resolveDispatcher } from './src/currentDispatcher';
 import { jsxDEV } from './src/jsx';
 
 export const useState: Dispatcher['useState'] = (initialState: any) => {
-	const dispatcher = resolveDispatcher()
-	return dispatcher.useState(initialState)
-}
+	const dispatcher = resolveDispatcher();
+	return dispatcher.useState(initialState);
+};
 
 export default {
 	version: '0.0.0',
